ee0c38e092bbb78ab1cb2daa0f9287ccdd5a6077
Kuniyoshi SAKAI <s1f101800328@iniad.org>
2020-03-29 22:14:18 +0900

    Initial Commit Message

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,9 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+Refile.secret_key = 'a38f2e9d01544add6812ef62085d66aa2d2a3508d8be3f0bb5cca3f1d4911429bed4a11c0b5905b52e297d8eb3b9df634eee66c6ae802345ddb80fd68bbd146b'
---
+++ db/migrate/20200318135303_remove_each_from_list.rb
@@ -0,0 +1,5 @@
+class RemoveEachFromList < ActiveRecord::Migration[5.2]
+  def change
+    remove_column :lists, :name, :string
+  end
+end
